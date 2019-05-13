e94bdc91e5d46b9133b61731234394d36b3ddb7a
verdantpath <m_dittmann@yahoo.com>
2019-05-12 19:48:11 -0700

    refactor buttons

--- parent-node/app.js
+++ parent-node/app.js
@@ -10,9 +10,27 @@ const addItemButton = document.querySelector('button.addItemButton');
 
 listUl.addEventListener('click', (event) => {
   if (event.target.tagName == 'BUTTON') {
-    let li = event.target.parentNode;
-    let ul = li.parentNode;
-    ul.removeChild(li);
+    if (event.target.className == 'remove') {
+      let li = event.target.parentNode;
+      let ul = li.parentNode;
+      ul.removeChild(li);
+    }
+    if (event.target.className == 'up') {
+      let li = event.target.parentNode;
+      let prevLi = li.previousElementSibling;
+      let ul = li.parentNode;
+      if (prevLi) {
+        ul.insertBefore(li, prevLi);
+      }
+    }
+    if (event.target.className == 'down') {
+      let li = event.target.parentNode;
+      let ul = li.parentNode;
+      let nextLi = li.nextElementSibling;
+      if (nextLi) {
+        ul.insertBefore(li, nextLi.nextElementSibling);
+      }
+    }
   }
 });
 
